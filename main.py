f0e53c1e226b65bfa1df3e41f7ce59df330e8ea4
Valentina <valentinagalyam@gmail.com>
2021-07-30 14:27:01 +0200

    deleted

--- main.py
+++ main.py
@@ -1,15 +1,2 @@
 from pprint import pprint
 import requests
-url = 'https://www.google.ru'
-
-response = requests.get(url)
-print()
-
-response.status_code
-response.ok
-response.headers
-response.content
-
-
-
-response.text
